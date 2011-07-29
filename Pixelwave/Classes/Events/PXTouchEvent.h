e009a1492e3cb6384513de1713191652da609086
John <john@spiralstormgames.com>
2011-07-29 16:49:20 -0400

    Fixed comments

--- Pixelwave/Classes/Events/PXTouchEvent.h
+++ Pixelwave/Classes/Events/PXTouchEvent.h
@@ -71,12 +71,12 @@ PXExtern NSString * const PXTouchEvent_TouchCancel;
 @property (nonatomic, readonly) UITouch *nativeTouch;
 
 /**
- *	Indicates if the touch which triggered this event has been captured by the target.
- *	The object which captured the event will usually be a PXInteractiveObject for
- *	for which the PXInteractiveObject::captureTouches property has been set to
- *	<code>YES</code>. If <code>captured</code> equals <code>YES</code>, the
- *	PXTouchEvent::target property will represent the object which captured this
- *	touch.
+ *	Indicates if the touch which triggered this event has been captured by the
+ *	target. The object which captured the event will usually be a
+ *	PXInteractiveObject for which the PXInteractiveObject::captureTouches
+ *	property has been set to <code>YES</code>. If <code>captured</code> equals
+ *	<code>YES</code>, the PXTouchEvent::target property will represent the
+ *	object which captured this touch.
  */
 @property (nonatomic, readonly) BOOL captured;
 
