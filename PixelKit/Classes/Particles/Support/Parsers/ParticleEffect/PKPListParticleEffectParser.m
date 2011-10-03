dab9b49f39bbbdc0447bdfb65b8b7f02b28e996b
Oz <oz@pixelwave.org>
2011-10-03 17:37:27 -0400

    Updated all headers to have the Pixelwave header

--- PixelKit/Classes/Particles/Support/Parsers/ParticleEffect/PKPListParticleEffectParser.m
+++ PixelKit/Classes/Particles/Support/Parsers/ParticleEffect/PKPListParticleEffectParser.m
@@ -17,13 +17,13 @@
  *                 ./__________|----'  .
  *            ,(   ___.....-,~-''-----/   ,(            ,~            ,(        
  * _.-~-.,.-'`  `_.\,.',.-'`  )_.-~-./.-'`  `_._,.',.-'`  )_.-~-.,.-'`  `_._._,.
- *
+ * 
  * Copyright (c) 2011 Spiralstorm Games http://www.spiralstormgames.com
- *
+ * 
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
  * arising from the use of this software.
- *
+ * 
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
