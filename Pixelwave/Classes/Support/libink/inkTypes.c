a7c8726917b4a12a446a83fecb90fb67254196de
John <john@spiralstormgames.com>
2011-11-17 18:25:02 -0500

    Added the c file for types to have constant values.

---
+++ Pixelwave/Classes/Support/libink/inkTypes.c
@@ -0,0 +1,18 @@
+//
+//  inkTypes.c
+//  ink
+//
+//  Created by John Lattin on 11/17/11.
+//  Copyright (c) 2011 Spiralstorm Games. All rights reserved.
+//
+
+#include "inkTypes.h"
+
+const inkSpreadMethod inkSpreadMethodDefault = _inkSpreadMethodDefault;
+const inkInterpolationMethod inkInterpolationMethodDefault = _inkInterpolationMethodDefault;
+const inkGradientType inkGradientTypeDefault = _inkGradientTypeDefault;
+const inkPathWinding inkPathWindingDefault = _inkPathWindingDefault;
+const inkTriangleCulling inkTriangleCullingDefault = _inkTriangleCullingDefault;
+const inkJointStyle inkJointStyleDefault = _inkJointStyleDefault;
+const inkLineScaleMode inkLineScaleModeDefault = _inkLineScaleModeDefault;
+const inkCapsStyle inkCapsStyleDefault = _inkCapsStyleDefault;
